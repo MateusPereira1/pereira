4b0b3097d7dd2acbbfed47d85bfc91ba3653e5a6
Mateus Pereira <118226023+mateuscromado@users.noreply.github.com>
2024-07-11 20:35:53 -0300

    Adição de elementos e otimizações

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 
-const observador = new IntersectionObserver(function(entries){
-    console.log(entries)
 
-    entries.forEach((entry)=>{
+var elementos = document.querySelector(".titulo-1");
 
-        if(entry.isIntersecting){
-            elementos.classList.add("ver")
-        }else{
-            elementos.classList.remove("ver")
+const observador = new IntersectionObserver(function (entries) {
+    console.log(entries);
+
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("ver");
+        } else {
+            entry.target.classList.remove("ver");
         }
+    });
 
-    })
+});
 
-})
+// Ensure that `elementos` is defined before observing
+if (elementos) {
+    observador.observe(elementos);
+}
 
-var elementos = document.querySelector(".titulo-1")
-
-observador.observe(elementos)
 
 
+//--------------------------------------------------------
+
+
+const observador1 = new IntersectionObserver(function (entries) {
+    console.log(entries);
+
+    entries.forEach((entry) => {
+        const elemento1 = entry.target;
+
+        if (entry.isIntersecting) {
+            setTimeout(()=>{
+            elemento1.classList.add("ver");
+
+            },200)
+        } else {
+            elemento1.classList.remove("ver");
+        }
+    });
+});
+
+// Seleciona todos os elementos com a classe "titulo-1"
+var elementos = document.querySelectorAll(".sol-box");
+
+// Observa cada um dos elementos selecionados
+elementos.forEach((elemento) => {
+    observador1.observe(elemento);
+});
